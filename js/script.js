a7536d7125eb26c1e00ee116147303a093942096
Stanley Su <stanleysu00@gmail.com>
2017-10-19 16:40:58 +1100

    added main variables

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 
 /* CODE STRUCTURE:
  *
- * 
+ * VARIABLES
  *
  */
 
 const animation = (() => {
-	
+
+	// VARIABLES
+
+	const canvas = document.getElementById('canvas');
+	const c = canvas.getContext('2d');
+	const mouse = { x: innerWidth / 2, y: innerHeight / 2 };
+	const base = { x: innerWidth / 2, y: innerHeight };
+	const gravity = 2;
 })();
