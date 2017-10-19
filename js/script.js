91b5ab5ba90b7c00e38610329dda894707a89cfb
Stanley Su <stanleysu00@gmail.com>
2017-10-19 16:41:44 +1100

    added constructors for cannon and projectiles

--- js/script.js
+++ js/script.js
@@ -2,12 +2,68 @@
 
 /* CODE STRUCTURE:
  *
+ * CONSTRUCTORS
  * VARIABLES
  *
  */
 
 const animation = (() => {
+// CONSTRUCTORS
 
+	class Cannon {
+		constructor(length, color) {
+			this.length = length;
+			this.color = color;
+			this.a;
+			this.baseX;
+			this.baseY;
+			this.endX;
+			this.endY;
+		}
+		update() {
+			this.a = Math.atan2(base.y - mouse.y, mouse.x - base.x);
+			this.baseX = base.x;
+			this.baseY = base.y;
+			this.endX = base.x + this.length * Math.cos(this.a);
+			this.endY =	base.y - this.length * Math.sin(this.a);
+			this.draw();
+		}
+		draw() {
+			c.beginPath();
+			c.moveTo(this.baseX, this.baseY);
+			c.lineTo(this.endX, this.endY);
+			c.lineWidth = 10;
+			c.strokeStyle = this.color;
+			c.stroke();
+		}
+	}
+
+	// constructor for projectiles
+	class Projectile {
+		constructor(x, y, v, a, r, color) {
+			this.x = x;
+			this.y = y;
+			this.vx = v;
+			this.vy = v;
+			this.a = a;
+			this.r = r;
+			this.g = gravity;
+			this.color = color;
+		}
+		update() {
+			this.vy -= this.g;
+			this.x += this.vx * Math.cos(this.a);
+			this.y -= this.vy * Math.sin(this.a);
+			this.draw();
+		}
+		draw() {
+			c.beginPath();
+			c.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+			c.fillStyle = this.color;
+			c.fill();
+		}
+	}
+	
 	// VARIABLES
 
 	const canvas = document.getElementById('canvas');
